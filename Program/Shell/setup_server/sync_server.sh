890d3d5068d39542f8c51d9260107c899a53de7b
rezmir99 <rezmir99@gmail.com>
2026-02-08 11:03:04 +0330

    Update via script

---
+++ Program/Shell/setup_server/sync_server.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+. "$HOME/server_profile.sh"
+
+# delete broken symlink
+find /usr/local/bin -xtype l -delete
+find /usr/local/lib -xtype l -delete
+
+# SERVER PROFILE
+cp -af "$PERM_GIT_PATH/ProgramData/server_profile.sh" "$HOME/server_profile.sh"
+
+# chmod and link
+## sh
+find "$PERM_GIT_PATH/Program/Shell" \( -name '*.lib.sh' \) -prune -o -type f -name '*.sh' -print | while IFS= read -r line; do
+    chmod 755 "$line"
+    line_name=${line##*/}
+    ln -sf "$line" "/usr/local/bin/${line_name%.sh}"
+done
+
+## lib.sh
+find "$PERM_GIT_PATH/Program/Shell" -type f -name '*.lib.sh' -print | while IFS= read -r line; do
+    line_name=${line##*/}
+    ln -sf "$line" "/usr/local/lib/$line_name"
+done
+
+## phyton
+find "$HOME/Program/Python" \( -name '*.lib.py' \) -prune -o -type f -name '*.py' -print | while IFS= read -r line; do
+    chmod 755 "$line"
+    line_name=${line##*/}
+    ln -sf "$line" "/usr/local/bin/${line_name%.py}"
+done
