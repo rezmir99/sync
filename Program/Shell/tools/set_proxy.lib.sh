a8dfdfe6f0fc5dd61f3618b6791de4e3b6d6e726
rezmir99 <rezmir99@gmail.com>
2026-02-08 10:33:26 +0330

    Update via script

---
+++ Program/Shell/tools/set_proxy.lib.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+#. $HOME/lib/get_arch.lib.sh
+
+# if [ "$ENVIRONMENT" = wsl ]; then  
+#     export HOSTIP=$(ip route show | grep -i default | awk '{ print $3}')
+# else
+#     export HOSTIP='127.0.0.1'
+# fi
+
+export HOSTIP='127.0.0.1'
+
+set_proxy() {
+[ -z "$1" ] && { echo "no port spcified"; return 1; }
+export http_proxy="http://${HOSTIP}:$1"
+export HTTP_PROXY="http://${HOSTIP}:$1"
+export HTTPS_PROXY="http://${HOSTIP}:$1"
+export https_proxy="http://${HOSTIP}:$1"
+export NO_PROXY='localhost,127.0.0.1,::1'
+export no_proxy='localhost,127.0.0.1,::1'
+git config --global http.proxy "http://${HOSTIP}:$1"
+set_proxy_apt $1
+}
+
+clear_proxy(){
+unset http_proxy HTTPS_PROXY no_proxy NO_PROXY https_proxy HTTP_PROXY
+git config --global --unset http.proxy
+clear_proxy_apt
+}
+
+set_proxy_apt_func() {
+    sudo echo "Acquire::http::Proxy \"http://$HOSTIP:$1\";" > "/etc/apt/apt.conf.d/proxy.conf"   
+    sudo echo "Acquire::https::Proxy \"http://$HOSTIP:$1\";" >> "/etc/apt/apt.conf.d/proxy.conf"
+}
+
+set_proxy_apt() {
+sudo -E bash -c ". $HOME/lib/set_proxy.lib.sh; set_proxy_apt_func $1"
+}
+
+clear_proxy_apt() {
+sudo -E bash -c "rm -f /etc/apt/apt.conf.d/proxy.conf"
+}
---
+++ ProgramData/server_profile.sh
@@ -0,0 +1,6 @@
+set -a
+PERM_GIT_PATH=$HOME/sync
+PERM_GIT_URL='https://raw.githubusercontent.com/rezmir99/sync'
+set +a
+
+. /usr/local/lib/set_proxy.sh 2>/dev/null
