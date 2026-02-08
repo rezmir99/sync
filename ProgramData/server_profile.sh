93451ebbf99a2bfc496e63e2a8ff71b36d27ca87
rezmir99 <rezmir99@gmail.com>
2026-02-08 10:37:20 +0330

    Update via script

--- ProgramData/server_profile.sh
+++ ProgramData/server_profile.sh
@@ -1,6 +1,6 @@
 set -a
 PERM_GIT_PATH=$HOME/sync
-PERM_GIT_URL='https://raw.githubusercontent.com/rezmir99/sync'
+PERM_GIT_URL='https://www.github.com/rezmir99/sync.git'
 set +a
 
 . /usr/local/lib/set_proxy.sh 2>/dev/null
