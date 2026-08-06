ffeab61876e53d08b4dbac9fc20781eb2a710bea
rezmir99 <rezmir99@gmail.com>
2026-08-06 13:31:31 +0330

    Update via script

--- Program/Linux/xdns/firewall.sh
+++ Program/Linux/xdns/firewall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-iptables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-port 5300
-iptables -t nat -A PREROUTING -p tcp --dport 53 -j REDIRECT --to-port 5300
+sudo iptables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-port 5300
+sudo iptables -t nat -A PREROUTING -p tcp --dport 53 -j REDIRECT --to-port 5300
 
 sudo ip6tables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 5300
 sudo ip6tables -t nat -A PREROUTING -p tcp --dport 53 -j REDIRECT --to-ports 5300
